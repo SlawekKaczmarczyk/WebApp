1a82ee6f76bcaadebbd4171c4316c0141c15f1be
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-04-27 23:55:53 +0200

    Adding messages to Sing Up

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True) 
     # Python can be used in html by using {{}}
 
@@ -13,4 +15,23 @@ def logout():
 
 @auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be longer than 4 characters!', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be longer than 2 characters!', category='error')
+        elif password1 != password2:
+            flash('Password don\'t match!', category='error')
+        elif len(password1) < 8:
+            flash('Password must be longer than 8 characters!', category='error')
+        else:
+            flash('Account created!', category='success')
+
+
+
     return render_template("sign_up.html")
